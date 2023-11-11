b918854cc4af41428d8a2cb6fb57f052025ed6f8
Roman Molochkov <molochkov2017@gmail.com>
2023-11-11 23:05:08 +0300

    feat: fix style

--- build.sbt
+++ build.sbt
@@ -22,9 +22,12 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
       scalaTest,
       mongoScalaDriver,
-      akkaHttp,
+      "com.softwaremill.sttp.tapir" %% "tapir-core" % "0.17.0-M9",
+      "com.softwaremill.sttp.tapir" %% "tapir-akka-http-server" % "0.17.0-M9",
       "com.typesafe" % "config" % "1.4.1",
       "ch.qos.logback" % "logback-classic" % "1.2.6",
-      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
+      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
+
+      "com.github.swagger-akka-http" %% "swagger-akka-http" % "2.2.0"
     )
   )
--- src/main/scala/API.scala
+++ src/main/scala/API.scala
@@ -2,19 +2,34 @@ package urlshortener
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.server.Route
 import com.mongodb.client.result.DeleteResult
 import java.net.URL
 import scala.concurrent.duration.Duration
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
+import sttp.tapir.{Endpoint, endpoint, query, stringBody}
+import sttp.tapir.server.akkahttp._
+import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.model.StatusCodes
+
+// import com.github.swagger.akka.SwaggerHttpService
+// import com.github.swagger.akka.model.Info
+
+// class SwaggerDocService(serverAddress: String, serverPort: Int) extends SwaggerHttpService {
+//   override val apiClasses: Set[Class[_]] = Set(classOf[ApiService])
+//   override val host: String = s"$serverAddress:$serverPort"
+//   override val info: Info = Info(version = "1.0")
+
+//   val route: Route = ???
+// }
+
 
 class ApiService(serverAddress: String, serverPort: Int, us: UrlShortener) {
 
   implicit val system: ActorSystem = ActorSystem("url-shortener-system")
 
-  def deleteResultMessage(result: DeleteResult): String = {
+   def deleteResultMessage(result: DeleteResult): String = {
     if (result.wasAcknowledged()) {
       s"Deleted ${result.getDeletedCount} document(s)."
     } else {
@@ -22,31 +37,55 @@ class ApiService(serverAddress: String, serverPort: Int, us: UrlShortener) {
     }
   }
 
-  val route =
-    path("getShort") {
-      parameter("url") { urlString =>
+  // Tapir endpoints
+  val getShortEndpoint: Endpoint[String, Unit, String, Any] =
+    endpoint.get
+      .in("getShort")
+      .in(query[String]("url"))
+      .out(stringBody)
+
+  val deleteByUrlEndpoint: Endpoint[String, Unit, String, Any] =
+    endpoint.delete
+      .in("deleteByUrl")
+      .in(query[String]("url"))
+      .out(stringBody)
+
+  val deleteByShortEndpoint: Endpoint[String, Unit, String, Any] =
+    endpoint.delete
+      .in("deleteByShort")
+      .in(query[String]("short"))
+      .out(stringBody)
+
+  // Route conversion
+  val getShortRoute: Route =
+    getShortEndpoint.toRoute(urlString =>
+      Future.successful(Right({
         val short = us.shortenUrl(new URL(urlString))
-        complete(s"http://$serverAddress:$serverPort/$short")
-      }
-    } ~
-      path("deleteByUrl") {
-        parameter("url") { urlString =>
-          val deleteResult = us.deleteByUrl(urlString)
-          complete(deleteResultMessage(deleteResult))
-        }
-      } ~
-      path("deleteByShort") {
-        parameter("short") { short =>
-          val deleteResult = us.deleteByShort(short)
-          complete(deleteResultMessage(deleteResult))
-        }
-      } ~
+        s"http://$serverAddress:$serverPort/$short\n"
+      }))
+    )
+
+  val deleteByUrlRoute: Route =
+    deleteByUrlEndpoint.toRoute(urlString => Future.successful(Right(deleteResultMessage(us.deleteByUrl(urlString)))))
+
+  val deleteByShortRoute: Route =
+    deleteByShortEndpoint.toRoute(short => Future.successful(Right(deleteResultMessage(us.deleteByShort(short)))))
+
+  //val swaggerDocService = new SwaggerDocService(serverAddress, serverPort)
+
+  // Combined route including the redirection
+  val route :Route = concat(
+    getShortRoute,
+      deleteByUrlRoute,
+      deleteByShortRoute,
+      //swaggerDocService.route,
       path(Segment) { short =>
         us.getUrl(short) match {
           case Some(url) => redirect(Uri(url.toString()), StatusCodes.PermanentRedirect)
           case None      => complete(StatusCodes.NotFound, "Short URL not found.")
         }
       }
+  )
 
   val serverFuture = Http().newServerAt(serverAddress, serverPort).bind(route)
   val serverBinding = Await.result(serverFuture, Duration.Inf)
